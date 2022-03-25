2d7719941416c234647dc6ccc6b8e34f8d969e8b
Leyla2105 <99959803+Leyla2105@users.noreply.github.com>
2022-03-25 09:58:26 +0300

    Add files via upload

---
+++ form_1 urok_6.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace urok__6
+{
+    public partial class Form1 : Form
+    {
+        private int counter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = "Старт";
+            button2.Text = "Выход";
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pct.Left = pct.Left + 20;
+            button1.Text = "Стоп";
+            pct.Left += 20;
+            if (pct.Right >= this.Width - 30)
+            {
+                button1.Text = "Старт";
+                timer1.Stop();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            counter++;
+            if (counter % 2 != 0)
+            {
+                pct.Location = new Point(12, 12);
+                timer1.Start();
+            }
+            if (counter % 2 == 0)
+            {
+                timer1.Stop();
+                button1.Text = "Старт";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
